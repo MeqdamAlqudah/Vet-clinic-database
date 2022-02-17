485d732efdaa9a71561437d89ae3ff9549e82422
MeqdamAlqudah <memuq56@gmail.com>
2022-02-17 21:50:07 +0200

    Try to fix issues with submit by the code Reviewer

--- data.sql
+++ data.sql
@@ -1,17 +1,3 @@
-INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
- VALUES ('Agumon','2020-03-02',0,TRUE,10.23),
- ('Gabumon','2018-11-15',2,TRUE,8),
- ('Pikachu','2021-01-07',1,FALSE,15.04),
- ('Devimon','2017-12-05',5,TRUE,11);
-INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
- VALUES ('Charmander','2020-02-08',0,FALSE,11);
-INSERT INTO animals (name,date_of_birth,weight_kg,neutered,escape_attempts)
- VALUES('Plantmon','2022-11-15',-5.7,TRUE,2),
-('Squirtle','1993-04-02',-12.13,FALSE,3),
-('Angemon','2005-06-12',-45,TRUE,1),
-('Boarmon','2005-06-07',20.4,TRUE,7),
-('Blossom','1998-10-13',17,TRUE, 3);
-
 BEGIN TRANSACTION;
 UPDATE animals
 SET species='unspecified' ;
@@ -24,19 +10,23 @@ SET species='digimon' WHERE name LIKE '%mon' ;
 UPDATE animals
 SET species='pokemon' WHERE species IS NULL;
 COMMIT TRANSACTION;
+SELECT species FROM animals;
 
 
 BEGIN TRANSACTION;
 DELETE FROM animals;
 ROLLBACK TRANSACTION;
+SELECT species FROM animals;
 
 
 BEGIN TRANSACTION;
-DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+DELETE FROM animals WHERE date_of_birth >= '2022-01-01';
 SAVEPOINT beforeChange;
 UPDATE animals
 SET weight_kg = weight_kg*-1;
 ROLLBACK TO beforeChange;
+SELECT COUNT(*) FROM ANIMALS;
 UPDATE animals
 SET weight_kg = weight_kg*-1 WHERE weight_kg<0;
 COMMIT TRANSACTION;
+SELECT species FROM animals;
