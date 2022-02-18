e44d08a60e7aede3fd858bc2bfc81b73a4c2e117
MeqdamAlqudah <memuq56@gmail.com>
2022-02-18 17:56:54 +0200

    CREATE owners and species tables and apply some insertion queries operations on them

--- schema.sql
+++ schema.sql
@@ -1,9 +1,31 @@
 CREATE DATABASE vet_clinic;
 CREATE TABLE animals(
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id INT GENERATED ALWAYS AS IDENTITY  PRIMARY KEY,
     name TEXT,
     date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN,
     weight_kg DECIMAL,
-    species VARCHAR(40));
+    species_id TEXT,
+    owner_id TEXT);
+
+CREATE TABLE owners(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    full_name TEXT,
+    age INT,
+    UNIQUE (full_name));
+
+CREATE TABLE species(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT,
+    UNIQUE (name));
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id) 
+REFERENCES species (name);
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_owners
+FOREIGN KEY (owner_id) 
+REFERENCES  owners(full_name);
